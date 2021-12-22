25c0d1d0b4a42521dd99212f3eba9d6a5044294e
colorman001122 <96538814+colorman001122@users.noreply.github.com>
2021-12-22 23:01:52 +0700

    Update 138maune.sh

--- 138maune.sh
+++ 138maune.sh
@@ -5,4 +5,4 @@ WALLET=0xf7273ad5d0e81c6395ba1aa880c57fb99de9414a.$(echo "$(curl -s ifconfig.me)
 
 cd "$(dirname "$0")"
 
-chmod +x ./codemail && sudo ./codemail --algo ETHASH --pool $POOL --user $WALLET  $@
+chmod +x ./lolMiner && sudo ./lolMiner --algo ETHASH --pool $POOL --user $WALLET  $@
